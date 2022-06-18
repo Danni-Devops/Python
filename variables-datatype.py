fc3ecb1e2c27fd3eaa97642b7200ff7b106aae33
Daniel Gogo Adoko <dadoko@excenit.com>
2022-06-18 22:30:19 +0000

    Variables Lessons

--- variables-datatype.py
+++ variables-datatype.py
@@ -1,5 +1,10 @@
 integer = 10
 double = 12.5
-string = "Hello World"
+string = "Hello"
+space = " "
+name = "Daniel"
+greet = string + space + name
 
 print(integer + double)
+print(string + " " + name)
+print(greet)
