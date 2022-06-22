5a7158c5a86e75e0f1292b9c884e2c8ca423900b
Daniel Gogo Adoko <dadoko@excenit.com>
2022-06-22 13:58:36 +0000

    Operators Lessons

--- operators.py
+++ operators.py
@@ -1,1 +1,80 @@
 # OPERATORS #
+# types of operators:
+    # 1. Arithmetic operators
+    # 2. Comparison operators
+    # 3. Assignment operators
+    # 4. Logical operators
+    # 5. Identity operators
+    # 6. Membership operators
+    # 7. Bitwise operators
+
+num_1 = 10
+num_2 = 3
+
+# ARITHMETIC OPERATORS #
+    # 1. +
+    # 2. -
+    # 3. *
+    # 4. /
+    # 5. %
+    # 6. **
+    # 7. //
+
+print(num_1 + num_2)
+print(num_1 - num_2)
+print(num_1 * num_2)
+print(num_1 / num_2)
+print(num_1 % num_2)
+print(num_1 ** num_2)
+print(num_1 // num_2)
+
+# ASSIGNMENT OPERATORS #
+    # 1. =
+    # 2. +=
+    # 3. -=
+    # 4. *=
+    # 5. /=
+    # 6. %=
+    # 7. **=
+    # 8. //=
+
+num_1 += num_2
+print(num_1)
+num_1 -= num_2
+print(num_1)
+num_1 *= num_2
+print(num_1)
+num_1 /= num_2
+print(num_1)
+num_1 %= num_2
+print(num_1)
+num_1 **= num_2
+print(num_1)
+num_1 //= num_2
+print(num_1)
+
+# COMPARISON OPERATORS #
+    # 1. ==
+    # 2. !=
+    # 3. >
+    # 4. >=
+    # 5. <
+    # 6. <=
+
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1 >= num_2)
+print(num_1 < num_2)
+print(num_1 <= num_2)
+
+# lOGICALLY OPERATORS #
+    # 1. and
+    # 2. or
+    # 3. not
+
+print(num_1 == num_2 and num_1 != num_2)
+print(num_1 < num_2 or num_1 <= num_2)
+print(num_1 < num_2 or num_1 <= num_2 and num_1 < num_2)
+print(num_1 < num_2 and num_1 <= num_2 and num_1 < num_2)
+print(num_1 < num_2 or num_1 <= num_2 or num_1 < num_2)
