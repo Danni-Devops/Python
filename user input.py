d89b1416454459111ddb9277c1539d6a5a198273
Daniel Gogo Adoko <dadoko@excenit.com>
2022-07-03 01:06:23 +0000

    User Input Lessons

---
+++ user input.py
@@ -0,0 +1,16 @@
+
+# python 3 not recognizing dataytpes
+num_1 = input("Enter First Number: ")
+num_2 = input("Enter Second Number: ")
+
+sum = num_1 + num_2
+
+print(sum)
+
+# using casting to fprce python 3 in recognizing dataytpes
+num_1 = int(input("Enter First Number: "))
+num_2 = int(input("Enter Second Number: "))
+
+sum = num_1 + num_2
+
+print(sum)
